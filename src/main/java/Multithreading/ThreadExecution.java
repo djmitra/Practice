5e1ab91a55b94d9d98f53a452b6960232593d358
Dibbojyoti <dibbojyoti.mitra@accenture.com>
2020-11-12 09:51:21 +0530

    Threading added

--- src/main/java/Multithreading/ThreadExecution.java
+++ src/main/java/Multithreading/ThreadExecution.java
@@ -1,37 +1,32 @@
 package main.java.Multithreading;
 
-class MyThread extends Thread{
+class MyThread extends Thread {
 
-    MyThread() {};
-    MyThread(Runnable r) {super(r);}
+    MyThread() {
+    }
 
-    public void run(){
+    ;
+
+    MyThread(Runnable r) {
+        super(r);
+    }
+
+    public void run() {
         System.out.println("running...");
     }
-    /*public static void main(String args[]){
-        MyThread t1=new MyThread();
-        t1.start();
-        t1.start();
-    }*/
 }
 
-class MyRunnable implements Runnable{
-    public void run(){
+class MyRunnable implements Runnable {
+    public void run() {
         System.out.println("thread is running...");
     }
-
-    /*public static void main(String args[]){
-        Multi3 m1=new Multi3();
-        Thread t1 =new Thread(m1);
-        t1.start();
-    }*/
 }
 
-    public class ThreadExecution {
-        public static void main(String args[]){
-            new MyThread().start();
-            new MyThread(new MyRunnable()).start();
-        }
+public class ThreadExecution {
+    public static void main(String args[]) {
+        new MyThread().start();
+        new MyThread(new MyRunnable()).start();
+    }
 }
 
 
